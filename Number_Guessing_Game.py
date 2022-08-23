8da011b57a960600af589d8d2068cb2ac158e72e
Asushei Balogun <victoriabalogun22@gmail.com>
2022-08-23 22:18:52 +0100

    initial commit

---
+++ Number_Guessing_Game.py
@@ -0,0 +1,4 @@
+import random
+number = random.randint(1,10)
+point = 5
+player_name = input("Hello there!\nWelcome to number guessing game. You have 5 points now but you lose a point for every incorrect guess.\n You have less than 5 tries to correctly guess the number I am thinking about.\n Let's Begin!. What should I call you?")
