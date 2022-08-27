db21787ba602dc45a2c6d3e0fe68b9f573c93e92
Asushei Balogun <victoriabalogun22@gmail.com>
2022-08-27 21:56:09 +0100

    while loop added

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -1,4 +1,18 @@
 import random
 number = random.randint(1,10)
 point = 5
-player_name = input("Hello there!\nWelcome to number guessing game. You have 5 points now but you lose a point for every incorrect guess.\n You have less than 5 tries to correctly guess the number I am thinking about.\n Let's Begin!. What should I call you?")
+player_name = input("Hello there!\nWelcome to number guessing game.\n"
+                    "You lose a point for every incorrect guess you make.\n"
+                    "You have 5 tries to correctly guess the number I am thinking about.\n"
+                    "Let's Begin!. What should I call you?")
+number_of_guesses = 0
+print ('okay! '+  player_name+ ' I am thinking about a number between 1 and 10. What is the number:')
+while number_of_guesses < 5:
+    guess = int (input())
+    number_of_guesses +=1
+    if guess < number: 
+        print('You guessed too low! Guess again:')
+    if guess > number: 
+        print('You guessed too high! Guess again:')
+    if guess == number: 
+        break        
